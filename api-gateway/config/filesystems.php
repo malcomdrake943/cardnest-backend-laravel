f90843d3344d85a5b7e508884f3cade1a0de8f09
Ali <m.alijaved69@gmail.com>
2026-06-12 09:05:23 +0500

    fixes in all filesystem.php

--- api-gateway/config/filesystems.php
+++ api-gateway/config/filesystems.php
@@ -53,12 +53,15 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'token' => env('AWS_SESSION_TOKEN'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+
+            'options' => [
+                'ACL' => '',
+            ],
         ],
 
     ],

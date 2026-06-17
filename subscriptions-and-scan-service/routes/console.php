58f0c3c6ad72f57da30617ec50e335fc03558c84
Ali <m.alijaved69@gmail.com>
2026-06-17 19:02:52 +0500

    made the cron job to run daily

--- subscriptions-and-scan-service/routes/console.php
+++ subscriptions-and-scan-service/routes/console.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+// Run the custom subscription renewal daily at midnight
+Schedule::command('subscriptions:renew-custom')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
